16c0f716409fd6ba4230f7437025375483f3fe21
Saul Miquilena <saul.miquilena@gmail.com>
2018-08-25 15:41:50 -0400

    Commit Inicial

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,93 @@
+const fs = require('fs');
+const colors = require('colors');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+
+    let data = JSON.stringify(listadoPorHacer);
+    fs.writeFile(`./db/data.json`, data, (err) => {
+        if (err) {
+            throw new Error('No se pudo grabar.', err).red;
+        } else {
+            console.log(`Se ha almacenado con éxito.`.green);
+        }
+    });
+
+}
+
+const cargarDB = () => {
+
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+
+}
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+
+    guardarDB();
+
+    return porHacer;
+
+}
+
+const getListado = (completado) => {
+
+    cargarDB();
+
+    let listadoFiltrado = listadoPorHacer.filter(tarea => tarea.completado === completado);
+
+    return listadoFiltrado;
+
+}
+
+const actualizar = (descripcion, completado = true) => {
+
+    cargarDB();
+
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+const borrar = (descripcion) => {
+
+    cargarDB();
+
+    let nuevoListado = listadoPorHacer.filter(tarea => tarea.descripcion !== descripcion);
+
+    if (nuevoListado.length === listadoPorHacer.length) {
+        return false;
+    } else {
+        listadoPorHacer = nuevoListado;
+        guardarDB();
+        return true;
+    }
+
+}
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
